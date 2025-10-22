374e3241ea0a840fe8d7bb67458276417131f192
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-10-22 14:38:28 -0400

    estruturando o blog

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className='navBar'>
+        <h2>
+            <Link to={'/'}>Blog</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li> 
+                <Link to={'/new'} className='newBtn'>New Post</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
